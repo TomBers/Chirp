a1fb1da05f4a75e1f7d0415a0a9bb04e4950f1c5
Tom Berman <tjcberman@gmail.com>
2020-12-18 09:23:01 +0000

    Chirp code taken from Chris McCord

---
+++ lib/chirp_web/live/post_live/post_component.ex
@@ -0,0 +1,29 @@
+defmodule ChirpWeb.PostLive.PostComponent do
+  use ChirpWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+      <div class="container">
+        <%= @post.body %> |
+        <a href="#" phx-click="like" phx-target="<%= @myself %>">
+        Like (<%= @post.likes_count %>)
+        </a> |
+        <a href="#" phx-click="repost" phx-target="<%= @myself %>">
+        Repost (<%= @post.reposts_count %>)
+        </a> |
+        <span><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, @post) %></span> |
+        <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></span>
+      </div>
+    """
+  end
+
+  def handle_event("like", _, socket) do
+    Chirp.Timeline.inc_likes(socket.assigns.post)
+    {:noreply, socket}
+  end
+
+  def handle_event("repost", _, socket) do
+    Chirp.Timeline.inc_repost(socket.assigns.post)
+    {:noreply, socket}
+  end
+end
